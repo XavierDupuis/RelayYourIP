99f8e5bc19eb2c18003720d53c7c1ca3506a512f
XavierDupuis <github-xavierdupuis@outlook.com>
2023-12-26 12:43:52 -0500

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,8 @@
+echo "⚡  Setting up msmtp"
+envsubst < .msmtprc.template > /etc/msmtprc
+msmtp --version
+echo "⚡  Setting up cron job with schedule '$CRON_SCHEDULE'"
+echo "$CRON_SCHEDULE /app/check_and_update.sh" > /etc/crontabs/root
+crontab -l
+echo "⚡  Starting cron daemon"
+crond -l 2 -f
