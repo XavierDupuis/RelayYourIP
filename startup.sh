48efdf81b7713213a8bb9cfb73505a5636dd258d
XavierDupuis <xavier0978@hotmail.fr>
2023-12-28 17:43:46 -0500

    redirect to /proc/1/fd/1

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ echo "⚡  Setting up msmtp"
 envsubst < msmtprc.template > /etc/msmtprc
 msmtp --version
 echo "⚡  Setting up cron job with schedule '$CRON_SCHEDULE'"
-echo "$CRON_SCHEDULE /app/check_and_update.sh" > /etc/crontabs/root
+echo "$CRON_SCHEDULE /app/check_and_update.sh > /proc/1/fd/1 2>&1" > /etc/crontabs/root
 crontab -l
 echo "⚡  Starting cron daemon"
 crond -l 2 -f
