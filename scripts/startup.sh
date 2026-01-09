61f5bab3792047e20ca5d449a8380c040fa7f90a
Xavier Dupuis <github-xavierdupuis@outlook.com>
2026-01-09 14:09:27 -0500

    use utils in startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,16 +1,19 @@
-echo "⚡  Setting up msmtp"
+# Source common utilities
+. "$(dirname "$0")/utils.sh"
+
+log "Setting up msmtp"
 envsubst < msmtprc.template > /etc/msmtprc
 
-echo "⚡  Verifying configuration file config.yml"
+log "Verifying configuration file config.yml"
 if [ -f /app/config/config.yml ]; then
-    echo "🟢  Found $(yq eval '.actions | length' /app/config/config.yml -o=json | jq -r .) actions"
+    log "Found $(yq eval '.actions | length' /app/config/config.yml -o=json | jq -r .) actions"
 else
     touch /app/config/config.yml
-    echo "🟠  config.yml file not found or empty. Created an empty config.yml."
+    warn "config.yml file not found or empty. Created an empty config.yml."
 fi
 
-echo "⚡  Setting up cron job with schedule '$CRON_SCHEDULE'"
+log "Setting up cron job with schedule '$CRON_SCHEDULE'"
 echo "$CRON_SCHEDULE /app/scripts/check_and_update.sh > /proc/1/fd/1 2>&1" > /etc/crontabs/root
 
-echo "⚡  Starting cron daemon"
+log "Starting cron daemon"
 crond -l 2 -f
